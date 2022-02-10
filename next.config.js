f63083944e4c4d92f4191791a2de69a7f5b0976b
Ivan Niukanen <ivanniu2003@gmail.com>
2022-02-10 14:29:25 +0200

    d

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
+require('dotenv').config()
+
 module.exports = {
   reactStrictMode: true,
+  env: {
+    GOOGLEMAP_APIKEY: process.env.GOOGLEMAP_APIKEY,
+    MONGODB_URI: process.env.MONGODB_URI
+  },
 }
